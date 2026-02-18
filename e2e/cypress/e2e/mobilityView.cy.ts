10e77e1b49389d5ce14f7ea8b027d09b1b9cb269
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2026-02-18 14:18:18 +0100

    switched to xsl-view for test, using the right view

--- e2e/cypress/e2e/mobilityView.cy.ts
+++ e2e/cypress/e2e/mobilityView.cy.ts
@@ -6,11 +6,13 @@ describe('The mobilityDCAT-AP view', () => {
   it('displays some of the expected content', () => {
     cy.login()
     // load the view using the xsl-view formatter (which is based on the editor-config)
-    cy.visit('/srv/eng/catalog.search#/metadata/d2e57ef8-2bff-41bd-b2ff-3ceb90dc4dd9')
+    cy.visit('/srv/eng/catalog.search#/metadata/d2e57ef8-2bff-41bd-b2ff-3ceb90dc4dd9/formatters/xsl-view?root=div&view=default')
     // check content (to be completed)
-    cy.get('h1').contains('MobilityDCAT-AP test record')
-    cy.contains('Description of MobilityDCAT-AP test record')
-    cy.get('button').contains('Accidents and incidents')
-    cy.get('button').contains('Trans-European Road Network (TERN)')
+    cy.get('td').contains('MobilityDCAT-AP test record').siblings('th').contains('Title')
+    cy.get('td').contains('Description of MobilityDCAT-AP test record').siblings('th').contains('Description')
+    cy.get('td').contains('Publisher 1 name').siblings('th').contains('Name')
+
+    cy.get('td').contains('https://w3id.org/mobilitydcat-ap/releases/1.1.0/').siblings('th').contains('Identifier')
+    cy.get('td').contains('https://joinup.ec.europa.eu/collection/semic-support-centre/solution/dcat-application-profile-data-portals-europe/release/200').siblings('th').contains('Identifier')
   })
 })
